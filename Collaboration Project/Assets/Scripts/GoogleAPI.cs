624c9f6b8e4556e1e3c2e94ad72e122058473298
Zalron <33308793+Zalron@users.noreply.github.com>
2018-11-13 09:51:02 +1100

    Started creating Google API

---
+++ Collaboration Project/Assets/Scripts/GoogleAPI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoogleAPI : MonoBehaviour
+{
+    public RawImage img;
+    private string url;
+    public float lat;
+    public float lon;
+    LocationInfo li;
+    public int zoom = 14;
+    public int mapWidth = 640;
+    public int mapHeight = 640;
+    public enum mapType
+    {
+        roadmap,satellite,hybrid,terrain
+    }
+    public mapType mapSelected;
+    public int scale;
+    IEnumerator Map()
+    {
+        url = " " +
+    }
+	void Start () // Use this for initialization
+    {
+		
+	}
+	
+	void Update () // Update is called once per frame
+    {
+		
+	}
+}
